4c859be7e6d12fcacceec2c86f55f0623c04abb0
石渡拓海 <takumi-ishiwata@c-fo.com>
2023-05-23 16:52:58 +0900

    Style component

--- src/pages/sign-in/[...index].tsx
+++ src/pages/sign-in/[...index].tsx
@@ -1,5 +1,22 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="container mx-auto">
+      <p>hoge</p>
+      <SignIn
+        appearance={{
+          elements: {
+            rootBox: {
+              width: "100%",
+            },
+            card: {
+              boxShadow: "none",
+              width: "100%",
+            },
+          },
+        }}
+      />
+    </div>
+  );
 }
